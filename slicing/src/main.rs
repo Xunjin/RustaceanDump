969d689600f5f94439cd8fd35fdb0ed25c46b950
Xunjin <xunjin.coder@gmail.com>
2020-01-04 12:24:44 -0300

    starting chapter 5 and removing old gitignore file

--- slicing/src/main.rs
+++ slicing/src/main.rs
@@ -17,9 +17,13 @@ fn main() {
     let slice = &s[3..len];
     let slice = &s[3..];
 
+    let a = [1, 2, 3, 4 ,5];
+
+    let slice = &a[1..3];
+
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
